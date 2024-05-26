53ad1ed27d70e95b1901c9687aa0719d93394f23
newhand22207807 <newhand22207807@gmail.com>
2024-05-27 01:06:34 +0800

    20240527

--- app/src/main/java/com/example/appfinalproject/SignUp.java
+++ app/src/main/java/com/example/appfinalproject/SignUp.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.signup);
     }
+
 }
