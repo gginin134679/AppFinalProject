52bcfe7f796a18f02fe91a1431a068e3b65928bc
newhand22207807 <newhand22207807@gmail.com>
2024-05-30 13:52:35 +0800

    新增主頁內容跟登入內容

--- app/src/main/java/com/example/appfinalproject/Login.java
+++ app/src/main/java/com/example/appfinalproject/Login.java
@@ -2,13 +2,38 @@ package com.example.appfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    private final static String defaultEmail = "root@gmail.com";
+    private final static String defaultPassword = "root";
+    private EditText etLoginEmail;
+    private EditText etLoginPassword;
+    private Button btnLoginLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
+        etLoginEmail = findViewById(R.id.et_login_email);
+        etLoginPassword = findViewById(R.id.et_login_password);
+        btnLoginLogin = findViewById(R.id.btn_login_login);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etLoginEmail.getText().toString();
+                String password = etLoginPassword.getText().toString();
+                if(defaultEmail.equals(email) && defaultPassword.equals(password)) {
+                    Intent intent = new Intent();
+                    intent.setClass(Login.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        btnLoginLogin.setOnClickListener(listener);
     }
 }
--- app/src/main/java/com/example/appfinalproject/MainActivity.java
+++ app/src/main/java/com/example/appfinalproject/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.appfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
   //ShenLian Logging for the first commit;
+  private Button btnMainPersonal;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    btnMainPersonal = findViewById(R.id.btn_main_personal);
+    View.OnClickListener listener = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (v.getId() == R.id.btn_main_personal) {
+          Intent intent = new Intent();
+          intent.setClass(MainActivity.this, Login.class);
+          MainActivity.this.startActivity(intent);
+        }
+      }
+    };
+    btnMainPersonal.setOnClickListener(listener);
   }
 }
